201577b0657e003a06154cb3d7f7b9e18eb1ca3c
Oliver Jaison <oliverjaison99@gmail.com>
2021-03-17 15:22:42 +0000

    added function to remove columns with NaN from df

--- main.py
+++ main.py
@@ -6,22 +6,27 @@ import numpy as np
 import requests
 import os
 import csv
+import keras
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from keras import backend as K
+from keras.models import Sequential
+from keras.layers import Activation
+from keras.layers import Dropout
+from keras.layers import InputLayer
+from keras.layers.core import Dense, Flatten
+from keras.optimizers import Adam
+from keras.metrics import categorical_crossentropy 
 
 
 # Pulls data from websites and stores them in csv files
 def update_data():
-    positive_cases_csv_URL1 = "https://coronavirus.data.gov.uk/api/v1/data?filters=areaType=overview&structure=%7B%22areaType%22:%22areaType%22,%22areaName%22:%22areaName%22,%22areaCode%22:%22areaCode%22,%22date%22:%22date%22,%22newCasesBySpecimenDate%22:%22newCasesBySpecimenDate%22,%22cumCasesBySpecimenDate%22:%22cumCasesBySpecimenDate%22%7D&format=csv" 
-    positive_cases_csv_URL2 = "https://covid.ourworldindata.org/data/owid-covid-data.csv"
-    req1 = requests.get(positive_cases_csv_URL1)
-    req2 = requests.get(positive_cases_csv_URL2)
-    URL_content1 = req1.content
-    URL_content2 = req2.content
-    positive_cases_file1 = open("positive_cases1.csv", "wb")
-    positive_cases_file1.write(URL_content1)
-    positive_cases_file1.close()
-    positive_cases_file2 = open("positive_cases2.csv", "wb")
-    positive_cases_file2.write(URL_content2)
-    positive_cases_file2.close()
+    positive_cases_csv_URL = "https://covid.ourworldindata.org/data/owid-covid-data.csv"
+    req = requests.get(positive_cases_csv_URL)
+    URL_content = req.content
+    positive_cases_file = open("positive_cases.csv", "wb")
+    positive_cases_file.write(URL_content)
+    positive_cases_file.close()
 
 
 # This is purely for the sources of data that include data outside the UK as having international data would mean too much to parse through
@@ -56,28 +61,29 @@ def normalise_dataframe(df):
 
 
 update_data() 
-filter_data("positive_cases2.csv")
+filter_data("positive_cases.csv")
 # Grabs the current working directory where the csv files are stored
 working_dir = os.getcwd()
 # Reads the csv files into their respective dataframes
-pos_cases_df1 = pd.read_csv(os.path.join(working_dir, "positive_cases1.csv"))
-pos_cases_df2 = pd.read_csv(os.path.join(working_dir, "positive_cases2.csv"))
+pos_cases_df = pd.read_csv(os.path.join(working_dir, "positive_cases.csv"))
 
-# Columns 0, 1 and 2 are not useful as I am looking at the UK exclusively right now for first csv
-# pos_cases_df1 = pos_cases_df1.drop(["areaType", "areaName", "areaCode"], axis=1)
-# lim = numberofdays(pos_cases_df1.iloc[0][0])
-# daysSince = []
-# for i in range(lim, -1, -1):
-#     daysSince.append(i)
-# pos_cases_df1["daysSince"] = daysSince
 # Above code just adds a column to the dataframe that counts the number of days since the earliest data entry
 
 # Dropping unnecessary information for second dataframe
-pos_cases_df2.drop(pos_cases_df2.iloc[:, 0:3], inplace=True, axis=1)
-pos_cases_df2.drop(pos_cases_df2.iloc[:, 3:7], inplace=True, axis=1)
-pos_cases_df2.drop(pos_cases_df2.iloc[:, 5:], inplace=True, axis=1)
+pos_cases_df.drop(pos_cases_df.iloc[:, 0:3], inplace=True, axis=1)
+pos_cases_df.drop(pos_cases_df.iloc[:, 3:7], inplace=True, axis=1)
+pos_cases_df.drop(pos_cases_df.iloc[:, 5:], inplace=True, axis=1)
 
-print(pos_cases_df2.head(), "\n")
+# Add an easier metric to process the dates by counting the number of days since the earliest entry
+# daysSince = []
+# for i in range(len(pos_cases_df)):
+#     daysSince.append(i)
+# pos_cases_df["daysSince"] = daysSince
+
 # Normalize all the data in the dataframe
-normalise_dataframe(pos_cases_df2)
-print(pos_cases_df2.head())
+normalise_dataframe(pos_cases_df)
+daysSince = []
+for i in range(len(pos_cases_df)):
+    daysSince.append(i)
+pos_cases_df["daysSince"] = daysSince
+print(pos_cases_df.tail())
