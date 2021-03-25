feb20db9db1579d6d2354693aaf312bbec324b91
Oliver Jaison <oliverjaison99@gmail.com>
2021-03-25 15:17:33 +0000

    Finished website structure

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from bs4 import BeautifulSoup as bs4
-from datetime import date
+
 from sklearn import preprocessing
 import pandas as pd
 import numpy as np
@@ -7,9 +6,9 @@ import requests
 import os
 import csv
 import keras
+import tabulate
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-from keras import backend as K
 from keras.models import Sequential
 from keras.layers import Activation
 from keras.layers import Dropout
@@ -20,6 +19,10 @@ from keras.metrics import categorical_crossentropy
 
 
 # Pulls data from websites and stores them in csv files
+
+# In[2]:
+
+
 def update_data():
     positive_cases_csv_URL = "https://covid.ourworldindata.org/data/owid-covid-data.csv"
     req = requests.get(positive_cases_csv_URL)
@@ -30,6 +33,10 @@ def update_data():
 
 
 # This is purely for the sources of data that include data outside the UK as having international data would mean too much to parse through
+
+# In[3]:
+
+
 def filter_data(filename):
     uk = list()
     with open(filename, 'r') as readFile:
@@ -42,48 +49,457 @@ def filter_data(filename):
         writer = csv.writer(writeFile)
         writer.writerows(uk)  
 
-       
-# Counts the number of days since the earliest data entry
-def numberofdays(date_in_question):
-    start_date = date(2020, 1, 31)
-    dateq = date_in_question.split("-")
-    cdate = date(int(dateq[0]), int(dateq[1]), int(dateq[2]))
-    return (cdate - start_date).days
+
+# This will only take columns in the data frame with no NaNs
+
+# In[6]:
 
 
-# This will normalise all the data in a dataframe
-def normalise_dataframe(df):
-    for i in range(1, len(df.columns)):
-        maxi = max(df.iloc[:,i])
-        mini = min(df.iloc[:,i])
-        for j in range(len(df.iloc[:,0])):
-            df.iloc[j, i] = (df.iloc[j, i] - mini)/(maxi-mini)
+def no_NaNs(df):
+    data = []
+    for column in df.columns:
+        temp = [float(i) for i in df[column]]
+        if np.isnan(np.sum(np.array(temp))):
+            df.drop([column], inplace=True, axis=1)
+
+
+# This will only take columns that don't have a single repeating entry
+
+# In[7]:
+
+
+def no_repeat(df):
+    data = []
+    for column in df.columns:
+        if df[column].nunique() == 1:
+            df.drop([column], inplace=True, axis=1)
+
+
+# This will create the input array for the Neural Network
+
+# In[8]:
+
+
+def create_features(df, window_size, forecast):
+    features = []
+    labels = []
+    i = window_size;
+    while i < len(df.iloc[:, 0]) - forecast-1:
+        window = df.iloc[i - window_size: i, 1:]
+        window = np.array(window)
+        window = window.flatten()
+        labels.append(df.iloc[i + forecast, 0])
+        features.append(window)
+        i += 1
+    features = np.array(features)
+    labels = np.array(labels)
+    return features, labels
+
+
+# In[9]:
 
 
 update_data() 
+
+
+# In[10]:
+
+
 filter_data("positive_cases.csv")
+
+
 # Grabs the current working directory where the csv files are stored
+
+# In[11]:
+
+
 working_dir = os.getcwd()
+
+
 # Reads the csv files into their respective dataframes
+
+# In[12]:
+
+
 pos_cases_df = pd.read_csv(os.path.join(working_dir, "positive_cases.csv"))
+pos_cases_df.drop(pos_cases_df.iloc[:, 0:3], inplace = True, axis=1)
+dates = pos_cases_df["date"]
+pos_cases_df.drop(["date"], inplace = True, axis=1)
+pos_cases_df.drop(["tests_units"], inplace = True, axis=1)
 
-# Above code just adds a column to the dataframe that counts the number of days since the earliest data entry
 
-# Dropping unnecessary information for second dataframe
-pos_cases_df.drop(pos_cases_df.iloc[:, 0:3], inplace=True, axis=1)
-pos_cases_df.drop(pos_cases_df.iloc[:, 3:7], inplace=True, axis=1)
-pos_cases_df.drop(pos_cases_df.iloc[:, 5:], inplace=True, axis=1)
+# In[13]:
 
-# Add an easier metric to process the dates by counting the number of days since the earliest entry
-# daysSince = []
-# for i in range(len(pos_cases_df)):
-#     daysSince.append(i)
-# pos_cases_df["daysSince"] = daysSince
 
-# Normalize all the data in the dataframe
-normalise_dataframe(pos_cases_df)
+no_NaNs(pos_cases_df)
+
+
+# In[14]:
+
+
+no_repeat(pos_cases_df)
+
+
+# Code just adds a column to the dataframe that counts the number of days since the earliest data entry
+
+# In[15]:
+
+
 daysSince = []
 for i in range(len(pos_cases_df)):
     daysSince.append(i)
 pos_cases_df["daysSince"] = daysSince
-print(pos_cases_df.tail())
+
+
+# Here I make the feature and label matrices with a 7 day window and a 1 day forecast
+
+# In[16]:
+
+
+window_size = 7
+forecast = 1
+features1, labels1 = create_features(pos_cases_df, window_size, forecast)
+
+
+# Splitting the feature and label matrices into training and testing by 8:2. I will split the training further down into 8:2 again for validation.
+
+# In[17]:
+
+
+training_features1, testing_features1 = np.split(features1, [int(0.8*len(features1))])
+training_labels1, testing_labels1 = np.split(labels1, [int(0.8*len(labels1))])
+
+
+# In[18]:
+
+
+input_shape = training_features1.shape[1]
+
+
+# In[19]:
+
+
+NN_model = Sequential()
+# Input layer
+NN_model.add(Dense(128, input_dim=input_shape, kernel_initializer="normal", activation="relu"))
+# Hidden Layers
+NN_model.add(Dense(256, kernel_initializer="normal", activation="relu"))
+NN_model.add(Dense(256, kernel_initializer="normal", activation="relu"))
+NN_model.add(Dense(256, kernel_initializer="normal", activation="relu"))
+# Output layer 
+NN_model.add(Dense(1, kernel_initializer="normal", activation="linear"))
+
+NN_model.compile(loss="mean_absolute_error", optimizer="adam", metrics=["accuracy"])
+NN_model.summary()
+
+
+# In[20]:
+
+
+history1 = NN_model.fit(training_features1, training_labels1, epochs=500, validation_split=0.2)
+
+
+# In[22]:
+
+
+epochs = range(1, len(history1.epoch) + 1)
+# plt.plot(epochs, history.history['loss'], label='Training Loss')
+plt.plot(epochs, history1.history['accuracy'], label='Training Accuracy')
+# plt.plot(epochs, history.history['val_loss'], label='Validation Loss')
+plt.plot(epochs, history1.history['val_accuracy'], label='Validation Accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.title("Training for 1 day forecast")
+plt.legend()
+plt.show()
+
+
+# In[23]:
+
+
+x = [i for i in range(int(testing_features1[0,-1]+1), int(testing_features1[-1,-1]+2))]
+y = testing_labels1
+x_pred = testing_features1
+y_pred = NN_model.predict(x_pred)
+err1 = np.abs(y - y_pred)
+acc1 = np.ones(len(y)) - np.divide(err1, y)
+acc_bar1 = np.mean(acc1)
+rms1 = np.sqrt(np.mean((err1)**2))
+print("Average accuracy:", acc_bar1)
+print("RMS:", rms1)
+
+
+# In[25]:
+
+
+plt.plot(x, y, label="True Values")
+plt.plot(x, y_pred, label="Predicted Values")
+plt.xlabel("Number of Days Since 31/01/2020")
+plt.ylabel("Total number of cases in the UK")
+plt.title("1 Day forecast")
+plt.legend()
+plt.show()
+
+
+# Here I make a new feature and label matrix with a 7 day window and a 2 day forecast
+
+# In[70]:
+
+
+window_size = 7
+forecast = 2
+features2, labels2 = create_features(pos_cases_df, window_size, forecast)
+
+
+# I split the matrices down into training and testing using a 8:2 splitting
+
+# In[71]:
+
+
+training_features2, testing_features2 = np.split(features2, [int(0.8*len(features2))])
+training_labels2, testing_labels2 = np.split(labels2, [int(0.8*len(labels2))])
+
+
+# The model has to be trained again with this new data
+
+# In[72]:
+
+
+history2 = NN_model.fit(training_features2, training_labels2, epochs=500, validation_split=0.2)
+
+
+# In[73]:
+
+
+epochs = range(1, len(history2.epoch) + 1)
+# plt.plot(epochs, history.history['loss'], label='Training Loss')
+plt.plot(epochs, history2.history['accuracy'], label='Training Accuracy')
+# plt.plot(epochs, history.history['val_loss'], label='Validation Loss')
+plt.plot(epochs, history2.history['val_accuracy'], label='Validation Accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Loss/Accuracy')
+plt.title("Training for 2 day forecast")
+plt.legend()
+plt.show()
+
+
+# In[74]:
+
+
+x = [i for i in range(int(testing_features2[0,-1]+1), int(testing_features2[-1,-1]+2))]
+y = testing_labels2
+x_pred = testing_features2
+y_pred = NN_model.predict(x_pred) 
+err2 = np.abs(y - y_pred)
+acc2 = np.ones(len(y)) - np.divide(err2, y)
+acc_bar2 = np.mean(acc2)
+rms2 = np.sqrt(np.mean((err2)**2))
+print("Average accuracy:", acc_bar2)
+print("RMS:", rms2)
+
+
+# In[31]:
+
+
+plt.plot(x, y, label="True Values")
+plt.plot(x, y_pred, label="Predicted Values")
+plt.xlabel("Days Since first case entry")
+plt.ylabel("Total number of cases in the UK")
+plt.title("2 Day forecast")
+plt.legend()
+plt.show()
+
+
+# In[50]:
+
+
+window_size = 7
+forecast = 5
+features3, labels3 = create_features(pos_cases_df, window_size, forecast)
+
+
+# In[51]:
+
+
+training_features3, testing_features3 = np.split(features3, [int(0.8*len(features3))])
+training_labels3, testing_labels3 = np.split(labels3, [int(0.8*len(labels3))])
+
+
+# In[52]:
+
+
+history3 = NN_model.fit(training_features3, training_labels3, epochs=500, validation_split=0.2)
+
+
+# In[54]:
+
+
+epochs = range(1, len(history3.epoch) + 1)
+# plt.plot(epochs, history.history['loss'], label='Training Loss')
+plt.plot(epochs, history3.history['accuracy'], label='Training Accuracy')
+# plt.plot(epochs, history.history['val_loss'], label='Validation Loss')
+plt.plot(epochs, history3.history['val_accuracy'], label='Validation Accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Loss/Accuracy')
+plt.title("Training for 5 day forecast")
+plt.legend()
+plt.show()
+
+
+# In[55]:
+
+
+x = [i for i in range(int(testing_features3[0,-1]+1), int(testing_features3[-1,-1]+2))]
+y = testing_labels3
+x_pred = testing_features3
+y_pred = NN_model.predict(x_pred) 
+err3 = np.abs(y - y_pred)
+acc3 = np.ones(len(y)) - np.divide(err3, y)
+acc_bar3 = np.mean(acc3)
+rms3 = np.sqrt(np.mean((err3)**2))
+print("Average accuracy:", acc_bar3)
+print("RMS:", rms3)
+
+
+# In[60]:
+
+
+plt.plot(x, y, label="True Values")
+plt.plot(x, y_pred, label="Predicted Values")
+plt.xlabel("Days Since first case entry")
+plt.ylabel("Total number of cases in the UK")
+plt.title("5 Day forecast")
+plt.legend()
+plt.show()
+
+
+# In[64]:
+
+
+window_size = 7
+forecast = 10
+features10, labels10 = create_features(pos_cases_df, window_size, forecast)
+
+
+# In[65]:
+
+
+training_features10, testing_features10 = np.split(features10, [int(0.8*len(features10))])
+training_labels10, testing_labels10 = np.split(labels10, [int(0.8*len(labels10))])
+
+
+# In[66]:
+
+
+history10 = NN_model.fit(training_features10, training_labels10, epochs=500, validation_split=0.2)
+
+
+# In[67]:
+
+
+epochs = range(1, len(history10.epoch) + 1)
+# plt.plot(epochs, history.history['loss'], label='Training Loss')
+plt.plot(epochs, history10.history['accuracy'], label='Training Accuracy')
+# plt.plot(epochs, history.history['val_loss'], label='Validation Loss')
+plt.plot(epochs, history10.history['val_accuracy'], label='Validation Accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Loss/Accuracy')
+plt.title("Training for 10 day forecast")
+plt.legend()
+plt.show()
+
+
+# In[68]:
+
+
+x = [i for i in range(int(testing_features10[0,-1]+1), int(testing_features10[-1,-1]+2))]
+y = testing_labels10
+x_pred = testing_features10
+y_pred = NN_model.predict(x_pred) 
+err10 = np.abs(y - y_pred)
+acc10 = np.ones(len(y)) - np.divide(err10, y)
+acc_bar10 = np.mean(acc10)
+rms10 = np.sqrt(np.mean((err10)**2))
+print("Average accuracy:", acc_bar10)
+print("RMS:", rms10)
+
+
+# In[69]:
+
+
+plt.plot(x, y, label="True Values")
+plt.plot(x, y_pred, label="Predicted Values")
+plt.xlabel("Days Since first case entry")
+plt.ylabel("Total number of cases in the UK")
+plt.title("10 Day forecast")
+plt.legend()
+plt.show()
+
+
+# In[44]:
+
+
+window_size = 7
+forecast = 50
+features50, labels50 = create_features(pos_cases_df, window_size, forecast)
+
+
+# In[45]:
+
+
+training_features50, testing_features50 = np.split(features50, [int(0.8*len(features50))])
+training_labels50, testing_labels50 = np.split(labels50, [int(0.8*len(labels50))])
+
+
+# In[46]:
+
+
+history50 = NN_model.fit(training_features50, training_labels50, epochs=500, validation_split=0.2)
+
+
+# In[47]:
+
+
+epochs = range(1, len(history50.epoch) + 1)
+# plt.plot(epochs, history.history['loss'], label='Training Loss')
+plt.plot(epochs, history50.history['accuracy'], label='Training Accuracy')
+# plt.plot(epochs, history.history['val_loss'], label='Validation Loss')
+plt.plot(epochs, history50.history['val_accuracy'], label='Validation Accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Loss/Accuracy')
+plt.legend()
+plt.show()
+
+
+# In[48]:
+
+
+x = [i for i in range(int(testing_features50[0,-1]+1), int(testing_features50[-1,-1]+2))]
+y = testing_labels50
+x_pred = testing_features50
+y_pred = NN_model.predict(x_pred) 
+err50 = np.abs(y - y_pred)
+acc50 = np.ones(len(y)) - np.divide(err50, y)
+acc_bar50 = np.mean(acc50)
+rms50 = np.sqrt(np.mean((err50)**2))
+print("Average accuracy:", acc_bar50)
+print("RMS:", rms50)
+
+
+# In[49]:
+
+
+plt.plot(x, y, label="True Values")
+plt.plot(x, y_pred, label="Predicted Values")
+plt.xlabel("Days Since first case entry")
+plt.ylabel("Total number of cases in the UK")
+plt.title("50 Day forecast")
+plt.legend()
+plt.show()
+
+
+# In[ ]:
+
+
+
+
--- report.md
+++ report.md
@@ -11,9 +11,11 @@
 - [The Appendices](#The-Appendices)
 
 ## Description
-The problem presented is to design and build a Covid forecasting engine using a neural network. The metric to be forecasted here is the total number of positive cases recorded in the UK. That is to say, the cumulative number of cases in the UK since January 31st of 2020. The solution stated in this report involves building a deep neural network in ```Python``` using ```tensorflow``` modules and training the model using a section of the data from a regularly updated csv file. The performance of the model is then tested by inputting the previous seven days worth of data, predicting the total number of positive cases the next day and comparing the prediction with the true value taken from the csv file. 
+The problem presented is to design and build a Covid forecasting engine using a neural network. The metric to be forecasted here is the total number of positive cases recorded in the UK. That is to say, the cumulative number of cases in the UK since January 31st of 2020. The solution stated in this report involves building a deep neural network in ```Python``` using ```tensorflow``` modules and training the model using a section of the data from a regularly updated csv file. The performance of the model is then tested by inputting the previous seven days worth of data, predicting the total number of positive cases the next day and comparing the prediction with the true value taken from the csv file.
 
 In the following sections of this report, the choice for the source of data will be justified and and any preprocessing involved will be explained. The design of the deep neural network will also be explained and the results will be presented.
+
+All of the work done for this project can be found on [Github](https://github.com/OliverJaison/SNS_Assignment20-21).
 
 ## The Data
 The csv file is taken from [https://covid.ourworldindata.org/data/owid-covid-data.csv](https://covid.ourworldindata.org/data/owid-covid-data.csv). Initially, the data was taken from the [United Kingdom government website](https://coronavirus.data.gov.uk/api/v1/data?filters=areaType=overview&structure=%7B%22areaType%22:%22areaType%22,%22areaName%22:%22areaName%22,%22areaCode%22:%22areaCode%22,%22date%22:%22date%22,%22newCasesByPublishDate%22:%22newCasesByPublishDate%22,%22cumCasesByPublishDate%22:%22cumCasesByPublishDate%22%7D&format=csv). The difference between the two sources of data is that the former has a significantly larger set of parameters that could perhaps help better train the neural network model. The UK goverment website has only a set of 6 parameters per day one of which is the metric to be predicted. The "ourworldindata" website has over 50 parameters per day. While it is not necessary to use all of these parameters, should the performance of the neural network fall below reasonable accuracy then giving more parameters to learn from can maybe improve results.
@@ -102,7 +104,9 @@ pos_cases_df["daysSince"] = daysSince
 |             8 |           6 |                     0.118 |                   0.088 |              11.11 |           3 |
 |             8 |           0 |                     0.118 |                   0     |              11.11 |           4 |
 
-At this point all that is left is for the data to be formatted as an input to a neural network and to split the data into training and testing. The intention is to feed the model seven days worth of data and have it predict the total number of cases on the eighth day. In order to do this a sliding window is created. The size of this window is parameterized but the logic is that it will cover 7 rows in the dataframe (ignoring the first column), add these rows to a list. At this point the list is a 2 dimensional array and for simplicity, we want the input to the model to be 2 dimensional, therefore this list is flattened to become 1 dimensional. This then flattened list is appended to the feature matrix. Similarly, the row exactly n increments from the sliding window is added to the labels matrix. The parameter n is the number of days in advance the model will attempt to forecast. This means that a new feature and label matrix will have to be generated for a new value of n and the model will have to be trained again on these new matrices. 
+At this point all that is left is for the data to be formatted as an input to a neural network and to split the data into training and testing. The intention is to feed the model seven days worth of data and have it predict the total number of cases on the eighth day. In order to do this a sliding window is created. The size of this window is parameterized but the logic is that it will cover 7 rows in the dataframe (ignoring the first column), add these rows to a list. 
+
+At this point the list is a 2 dimensional array and for simplicity, we want the input to the model to be 2 dimensional, therefore this list is flattened to become 1 dimensional. This then flattened list is appended to the feature matrix. Similarly, the row exactly n increments from the sliding window is added to the labels matrix. The parameter n is the number of days in advance the model will attempt to forecast. This means that a new feature and label matrix will have to be generated for a new value of n and the model will have to be trained again on these new matrices. 
 
 ```bash
 def create_features(df, window_size, forecast):
@@ -129,7 +133,9 @@ training_labels1, testing_labels1 = np.split(labels1, [int(0.8*len(labels1))])
 ```
 
 ## The Neural Network Model
-As stated before, the chosen architecture for this project is a dense neural network model. Neural networks are commonly used for classification problems such as gender detection or symbol classification. The problem faced here is a regression problem because there are no discrete metrics being looked at, all the parameters are continuous. This means that common practices such as one hot encoding the input or output will not work here. The dense neural network architecture is the simplest by far, only using a series of dense layers for both input and hidden layers. The specific structure is shown below:
+As stated before, the chosen architecture for this project is a dense neural network model. Neural networks are commonly used for classification problems such as gender detection or symbol classification. The problem faced here is a regression problem because there are no discrete metrics being looked at, all the parameters are continuous. 
+
+This means that common practices such as one hot encoding the input or output will not work here. The dense neural network architecture is the simplest by far, only using a series of dense layers for both input and hidden layers. The specific structure is shown below:
 
 ```bash
 input_shape = training_features1.shape[1]
@@ -160,6 +166,17 @@ Total params: 169473,
 Trainable params: 169473,
 Non-trainable params: 0
 
+```py
+history1 = NN_model.fit(
+    training_features1, 
+    training_labels1, 
+    epochs=500, 
+    validation_split=0.2
+    )
+```
+
+The activation function for the output layer is made to be linear because this is a regresson problem and the numerical values of the output are what is important. Within the hidden layer, ReLu is used as the activation function because it is simple and cheap in terms of processing power. Additionally, we will eventually get quite large inputs looking at later entries of the data set so it would be best if the slope does not saturate. 
+
 ## The Results
 One problem that was encountered with compiling the model was that accuracy was not the best metric to use. Due to the problem being of regressive nature rather than classification, the model can make a prediction with only 0.5% error and the accuracy would still be zero because the prediction is not exactly correct. This would mean that even after training for over 500 epochs, the training and validation accuracy will remain poor. This can be seen in the following graphs:
 
@@ -181,17 +198,27 @@ acc_bar1 = np.mean(acc1)
 
 The feature and label matrices are regenerated for 2, 5, 10, 20 and 50 day forecasts and the model is trained against each of these datasets to show the performance.
 
-|   Number of days in advance|  Average accuracy|
-|---------------------------:|-----------------:|
-|                          1 |           84.86% |
-|                          2 |           82.84% |
-|                          5 |           83.57% |
-|                         10 |           80.54% |
-|                         20 |           77.25% |
-|                         50 |           63.73% |
+![Table of Results](Results.PNG)
 
 ## The Conclusion
+The project involved using machine learning techniques to forecast the cumulative number of Covid cases in the UK. The data was imported from the [Our World in Data](https://covid.ourworldindata.org/data/owid-covid-data.csv) website and preprocessed by removing non-UK data entries and all columns with NaN values as well as all columns with constant repeating data entries. This data was then flattened and split with 80:20 splitting and used to train a Deep Neural Network model. The performance of the model gradually gets worse with more the more days in advance it attempts to forecast. The best possible average accuracy being 84.86%.
 
 ## The Appendices
-### Appendix A: Code
-### Appendix B: Graphs
+
+![Error for 1 day in advance](Error_1_day.PNG)
+![Training for 1 day in advance](Training_1_day.PNG)
+
+![Error for 2 day in advance](Error_2_day.PNG)
+![Training for 2 day in advance](Training_2_day.PNG)
+
+![Error for 5 day in advance](Error_5_day.PNG)
+![Training for 5 day in advance](Training_5_day.PNG)
+
+![Error for 10 day in advance](Error_10_day.PNG)
+![Training for 10 day in advance](Training_10_day.PNG)
+
+![Error for 20 day in advance](Error_20_day.PNG)
+![Training for 20 day in advance](Training_20_day.PNG)
+
+![Error for 50 day in advance](Error_50_day.PNG)
+![Training for 50 day in advance](Training_50_day.PNG)
